d6dd42aeb8e2b8b785a2dc9c364bc1fba9ee8c93
Mikhail Maslov <i@mihailmaslov.ru>
2023-10-25 21:49:07 +1000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/maslovmikhail/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardsList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+  evt.target.closest('.card').remove();
+}
 
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function (element) {
+  const cardElement = cardTemplate.cloneNode(true);
+  
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__title').textContent = element.name;
+
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+
+  cardsList.append(cardElement);
+});

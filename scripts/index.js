016f87fba66790d7c0185d791219b20c6fdf45e5
Mikhail Maslov <i@mihailmaslov.ru>
2023-10-28 23:10:56 +1000

    second commit

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,16 @@ const cardTemplate = document.querySelector('#card-template').content;
 const cardsList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(element) {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__image').alt = element.alt;
+  cardElement.querySelector('.card__title').textContent = element.name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+  
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
 function deleteCard(evt) {
@@ -12,14 +22,12 @@ function deleteCard(evt) {
 }
 
 // @todo: Вывести карточки на страницу
+function addCard() {
+  initialCards.forEach(function (element) {
+    const cardElement = createCard(element);
+    
+    cardsList.append(cardElement);
+  });
+}
 
-initialCards.forEach(function (element) {
-  const cardElement = cardTemplate.cloneNode(true);
-  
-  cardElement.querySelector('.card__image').src = element.link;
-  cardElement.querySelector('.card__title').textContent = element.name;
-
-  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
-
-  cardsList.append(cardElement);
-});
+addCard();
